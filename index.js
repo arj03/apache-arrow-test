5a97e115facea2e5125d23c4bf3af8cfd04b857f
Anders Rune Jensen <arj03@protonmail.ch>
2020-02-01 15:31:42 +0100

    Implement links using hacked version of backlinks

--- index.js
+++ index.js
@@ -18,6 +18,101 @@ function datediff(date1, date2) {
   return date1.getTime() - date2.getTime();
 }
 
+const ref = require('ssb-ref')
+const matchChannel = /^#[^\s#]+$/
+
+/*
+[ [ '@6CAxOI3f+LUOVrbAl0IemqiS7ATpQvr9Mdw9LC4+Uv0=.ed25519' ],
+  [ '#testing' ],
+  [ '%HZVnEzm0NgoSVfG0Hx4gMFbMMHhFvhJsG2zK/pijYII=.sha256',
+    '#testing' ],
+  [ '&xGsuLb0REV+6iDAy4QGvGxQvaIcRGZFiqBgXD8dw7aM=.sha256' ],
+  [ '&xwYBP2fBpyobN19U5RO+f9BwxwX/BWOAxXJ2Q8S9W+0=.sha256' ],
+  [ '@6CAxOI3f+LUOVrbAl0IemqiS7ATpQvr9Mdw9LC4+Uv0=.ed25519',
+    '&Il2SFDKScJcqt3CTl+ZaeIJLXGwmPbQHUTi9lVaUH5c=.sha256' ],
+  [ '%ZbyoNNv9dF0sjXngPjJAifwnMTWmlsXWyq0KLB7BoTQ=.sha256',
+    '%orkjiS9Rk37b4yQuyMnF08QwK8hL4X/wglUCtOflaWs=.sha256',
+    '#patchwork-dev' ],
+  [ '#patchwork-design' ],
+  [ '%1P6WqqYofmV7+494nQGBBg9S78/eFU9689py6zwOfs0=.sha256',
+    '%JSOpWWQ2b8QNZBSncz6G+ppSUHZIZJVI6jj4nV8Ce38=.sha256',
+    '#new-people' ],
+  [ '%2fUo4+ARCO8Ue8PKI324ZuxtO19BGppAzJ8VUxOZtNY=.sha256',
+    '%wNkP/PymJa3nrJH+NhsNFbFHQBD6o1t6qKssF1pK3+E=.sha256',
+    '#patchwork-design' ] ]
+*/
+
+var extractRegex = /([@%&][A-Za-z0-9\/+]{43}=\.[\w\d]+)/
+var channelExtractRegex = /(#[A-Za-z0-9\/+]{30})/
+
+function type(id) {
+  if(typeof(id) !== 'string') return false
+  var c = id.charAt(0)
+  if (c == '@' && ref.isFeedId(id))
+    return 'feed'
+  else if (c == '%' && ref.isMsgId(id))
+    return 'msg'
+  else if (c == '&' && ref.isBlobId(id))
+    return 'blob'
+  else
+    return false
+}
+
+function extractLinks(msg) {
+  //  console.log("====")
+
+  /*
+  var links = new Set(extractRegex.exec(msg.value.content.text))
+
+  var channels = channelExtractRegex.exec(msg.value.content.text)
+  for (var i = 0; channels != null && i < channels.length; ++i)
+    links.add(`#${ref.normalizeChannel(channels[i].slice(1))}`)
+
+  for (var k in msg.value.content) {
+    var t = type(msg.value.content[k])
+    console.log(`type: ${t}, ${k}: ${msg.value.content[k]}`)
+    if (t == 'channel')
+      links.add(`#${ref.normalizeChannel(msg.value.content["channel"])}`)
+    else if (t)
+      links.add(msg.value.content[k])
+  }
+
+  return Array.from(links)
+  */
+
+  var links = new Set()
+  walk(msg.value.content, function (path, value) {
+    if (Array.isArray(path) && path[0] === 'channel') { // HACK: handle legacy channel mentions
+      var channel = ref.normalizeChannel(value)
+      if (channel)
+        value = `#${channel}`
+    }
+
+    // TODO: should add channel matching to ref.type
+    if (type(value))
+      links.add(value)
+    else if (isChannel(value))
+      links.add(`#${ref.normalizeChannel(value.slice(1))}`)
+  })
+
+  return Array.from(links)
+}
+
+function isChannel (value) {
+  return typeof value === 'string' && value.length < 30 && matchChannel.test(value)
+}
+
+function walk (obj, fn, prefix) {
+  //console.log(`walking ${JSON.stringify(obj)}, prefix: ${prefix}`)
+  if (obj && typeof obj === 'object') {
+    for (var k in obj) {
+      walk(obj[k], fn, (prefix || []).concat(k))
+    }
+  } else {
+    fn(prefix, obj)
+  }
+}
+
 pull(
   db.stream(),
   pull.collect((err, messages) => {
@@ -31,10 +126,12 @@ pull(
     var sequences = messages.map(x => x.value.value.sequence)
     var types = messages.map(x => x.value.value.content.type)
 
-    // post specific
-    var channels = messages.map(x => x.value.value.content.channel)
-    var roots = messages.map(x => x.value.value.content.root ? x.value.value.content.root.toString() : null)
+    // channel and root "keys" are contained in links
 
+    var links = messages.map(x => extractLinks(x.value)) 
+
+    console.log(`extracted data ${datediff(new Date(), start)}`)
+   
     // indexes:
     // clock: [data.value.author, data.value.sequence] => flume seq
     // keys:  data.key => flume seq
@@ -88,6 +185,8 @@ pull(
     console.log(`query ${today.length} results, ${datediff(new Date(), queryStart)}`)
     */
 
+    var listChild = new arrow.Field('list[Utf8]', new arrow.Utf8())
+    
     var all = arrow.Table.new([
       arrow.DateVector.from(dates),
       arrow.Int32Vector.from(flumeseqs),
@@ -95,9 +194,8 @@ pull(
       arrow.Vector.from({ values: authors, type: new arrow.Dictionary(new arrow.Utf8(), new arrow.Int32()) }),
       arrow.Int32Vector.from(sequences),
       arrow.Vector.from({ values: types, type: new arrow.Dictionary(new arrow.Utf8(), new arrow.Int32()) }),
-      arrow.Vector.from({ values: channels, type: new arrow.Dictionary(new arrow.Utf8(), new arrow.Int32()) }),
-      arrow.Vector.from({ values: roots, type: new arrow.Dictionary(new arrow.Utf8(), new arrow.Int32()) }),
-    ], ["date", "flumeseq", "key", "author", "sequence", "type", "channel", "root"])
+      arrow.Vector.from({ values: links, type: new arrow.List(listChild) }),
+    ], ["date", "flumeseq", "key", "author", "sequence", "type", "links"])
     
     console.log(all.length)
     console.log(`imported data into arrow ${keys.length} keys, ${datediff(new Date(), start)}`)
@@ -114,6 +212,12 @@ pull(
 
     console.log(`query ${today2.length} results, ${datediff(new Date(), queryStart)}`)
     //console.log(today2)
+    
+    var q2 = Array.from(
+      all.filter(
+        arrow.predicate.col('links').eq('%GcvjVk+NLsjOB5Vd+vceGXEOeYmHoRA6lgXVJzPuMxw=.sha256')))
+
+    console.log(q2)
     
     var w = arrow.RecordBatchStreamWriter.writeAll(all)
     const fs = require('fs')
